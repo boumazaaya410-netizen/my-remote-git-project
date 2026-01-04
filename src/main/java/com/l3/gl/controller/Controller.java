db26c6c82712a088ad9cdb9dcc6ba048b0a353ba
AYA <boumazaaya410@gmail.com>
2026-01-04 10:15:46 +0100

    Add new Controller class

--- src/main/java/com/l3/gl/controller/Controller.java
+++ src/main/java/com/l3/gl/controller/Controller.java
@@ -1,21 +1,5 @@
 package com.l3.gl.controller;
 
-import com.l3.gl.Product;
-import com.l3.gl.User;
-// import com.l3.gl.view.Ui; // علقناه مؤقتًا إذا Ui غير موجودة
-
 public class Controller {
 
-    // private Ui view; // مؤقت
-    private Product product;
-    private User user;
-
-    public Controller(Product product, User user) {
-        this.product = product;
-        this.user = user;
-    }
-
-    public String sayHello() {
-        return "Hello from Controller";
-    }
 }
