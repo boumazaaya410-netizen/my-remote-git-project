1eba262ed40b68d60e8ca1571a26feb03cac06a9
AYA <boumazaaya410@gmail.com>
2026-01-04 08:53:44 +0100

    Add project files

---
+++ src/main/java/com/l3/gl/controller/controlle.java
@@ -0,0 +1,21 @@
+package com.l3.gl.controller;
+
+import com.l3.gl.Product;
+import com.l3.gl.User;
+// import com.l3.gl.view.Ui; // علقناه مؤقتًا إذا Ui غير موجودة
+
+public class controlle {
+
+    // private Ui view; // مؤقت
+    private Product product;
+    private User user;
+
+    public controlle(Product product, User user) {
+        this.product = product;
+        this.user = user;
+    }
+
+    public String sayHello() {
+        return "Hello from Controller";
+    }
+}
